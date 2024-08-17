c7ea44cd9cd03ff077a25e89396e40fb0425e160
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2024-08-17 13:12:16 +0100

    feat: update system

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -8,21 +8,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <main className="h-screen w-screen overflow-auto">
-          <div className="py-7 h-[100px] flex items-center ">
-            {/* min-[1440px]:w-[80%] */}
-            <div className=" w-[90%] mx-auto">
-              <HomeNavbar />
-            </div>
-          </div>
-          <ScrollArea data-state="hidden" className="">
-            <section className=" h-[calc(100vh-100px)] ">{children}</section>
-            <ScrollAreaScrollbar hidden={false} />
-          </ScrollArea>
-        </main>
-      </body>
-    </html>
+    <main className="h-screen w-screen overflow-auto">
+      <div className="py-7 h-[100px] flex items-center ">
+        {/* min-[1440px]:w-[80%] */}
+        <div className=" w-[90%] mx-auto">
+          <HomeNavbar />
+        </div>
+      </div>
+      <ScrollArea data-state="hidden" className="">
+        <section className=" h-[calc(100vh-100px)] ">{children}</section>
+        <ScrollAreaScrollbar hidden={false} />
+      </ScrollArea>
+    </main>
   );
 }
