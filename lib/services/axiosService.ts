cdf7ace67d441ce0332aba36f09835a0b60e411f
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2024-08-17 13:09:37 +0100

    feat: update

--- lib/services/axiosService.ts
+++ lib/services/axiosService.ts
@@ -1,36 +1,29 @@
 import axios from "axios";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/authOptions";
+import { authOptions } from "@/app/api/auth/authOptions";
 
-const axiosClient = axios.create({
+const axiosService = axios.create({
   baseURL: `${process.env.API_BASE_URL}`,
 });
+axiosService.defaults.headers.post["Content-Type"] = "application/json";
 
-axiosClient.defaults.headers.post["Content-Type"] = "multipart/form-data";
-
-axiosClient.interceptors.request.use(async (AxiosRequestConfig) => {
+axiosService.interceptors.request.use(async (AxiosRequestConfig) => {
   const session: any = await getServerSession(authOptions);
   let token;
   if (session) {
     token = session.jwt;
   }
   AxiosRequestConfig.headers.Authorization = `Bearer ${token}`;
-
   return AxiosRequestConfig;
 });
-
-axiosClient.interceptors.response.use(
+axiosService.interceptors.response.use(
   (AxiosResponse) => {
     return AxiosResponse;
   },
   (error) => {
     try {
-      console.log(error);
-    } catch (e) {
-      console.error(e);
-    }
+    } catch (e) {}
     throw error;
   }
 );
-
-export default axiosClient;
+export default axiosService;
