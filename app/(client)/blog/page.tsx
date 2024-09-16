b889ce88c1409918f290dbb1b5edc25dd8fe789a
josephDev123 <josephuzuegbu55@gmial.com>
2024-09-16 16:05:44 +0100

    blog page biulding

--- app/(client)/blog/page.tsx
+++ app/(client)/blog/page.tsx
@@ -4,6 +4,9 @@ import SearchSection from "./components/SearchSection";
 import PostTransactImge from "@/app/svgIconComponent/PosTransactImg";
 import PrimaryButton from "@/app/commons/PrimaryButtons";
 import MoreInfoPreviewCard from "./components/MoreInfoPreviewCard";
+import BlogPreviewCard from "./components/BlogPreviewCard";
+import { blogSampleDataset } from "@/app/data/blogSampleDataset";
+import { MoreInfoPreviewData } from "@/app/data/moreInfoPreviewData";
 
 export default function page() {
   return (
@@ -11,24 +14,17 @@ export default function page() {
       <div className="h-[350px] bg-red-300"></div>
       <div className="w-[80%] mx-auto">
         <SearchSection />
-        <div className="grid grid-cols-4">
-          <div className="grid grid-cols-2 gap-2 col-span-3 h-[268px]">
-            <div style={{ backgroundImage: "url('blogSampleImg.png')" }}></div>
-            <div className="flex flex-col p-2 space-y-4 bg-neutral-50">
-              <h1 className="font-bold text-lg">Escrow vs Direct Payment</h1>
-              <p className="text-lg">
-                Direct payments are quicker and simpler, escrow services provide
-                enhanced security and trust, making them a preferred choice for
-                higher-value or riskier transactions.
-              </p>
-
-              <PrimaryButton className=" py-1 self-end w-32 h-12 text-sm">
-                Read More
-              </PrimaryButton>
-            </div>
+        <div className="grid grid-cols-4 ">
+          <div className="col-span-3 space-y-8">
+            {blogSampleDataset.map((blog, i) => (
+              <BlogPreviewCard key={i} data={blog} />
+            ))}
           </div>
-          <div className="col-span-1 flex flex-col space-y-4">
-            <MoreInfoPreviewCard />
+
+          <div className="col-span-1 flex flex-col space-y-4 h-full">
+            {MoreInfoPreviewData.map((item, i) => (
+              <MoreInfoPreviewCard key={i} data={item} />
+            ))}
           </div>
         </div>
       </div>
---
+++ app/data/moreInfoPreviewData.ts
@@ -0,0 +1,40 @@
+import MoreInfoPreviewCard from "../(client)/blog/components/MoreInfoPreviewCard";
+import { MoreInfoPreviewDataType } from "../types.ts/moreInfoPreviewDataType";
+
+// export const MoreInfoPreviewData = {
+//   category: ["Security", "Real Estate", "Online Shopping", "Freelancing"],
+//   "Popular Tags": [
+//     "#freelancing",
+//     "#security",
+//     "#realestate",
+//     "#onlineshopping",
+//   ],
+//   "Recent Posts": [
+//     "Escrow vs Direct Payment",
+//     "Benefits of Using MIMOTAR For Real Estate",
+//     "The Future of Escrow Services",
+//     "Avoiding Scams with  MIMOTAR",
+//   ],
+// };
+
+export const MoreInfoPreviewData: MoreInfoPreviewDataType[] = [
+  {
+    category: ["Security", "Real Estate", "Online Shopping", "Freelancing"],
+  },
+  {
+    "Popular Tags": [
+      "#freelancing",
+      "#security",
+      "#realestate",
+      "#onlineshopping",
+    ],
+  },
+  {
+    "Recent Posts": [
+      "Escrow vs Direct Payment",
+      "Benefits of Using MIMOTAR For Real Estate",
+      "The Future of Escrow Services",
+      "Avoiding Scams with  MIMOTAR",
+    ],
+  },
+];
