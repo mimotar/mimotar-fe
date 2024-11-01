678208538aa8224815c0e1760444f56a6f15cc44
josephDev123 <josephuzuegbu55@gmail.com>
2024-11-01 12:15:43 +0100

    doughnut chart

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,52 @@
+import PrimaryButton, { PrimaryOutline } from "@/app/commons/PrimaryButtons";
+import Card from "./(components)/Card";
+import DoughnutChart from "./(components)/DoughnutChart";
+
 export default function page() {
-  return <div>page</div>;
+  return (
+    <section className="bg-white flex flex-col h-full w-full 2xl:p-10 p-8 overflow-y-auto">
+      <div className="flex justify-between items-center gap-2 flex-wrap">
+        <div className="flex flex-col">
+          <h1 className="font-bold text-2xl">Welcome back, Olawale</h1>
+          <p className="text-neutral-900 text-sm">
+            Stay updated with the overview of your escrow transactions.
+          </p>
+        </div>
+        <PrimaryButton className="w-fit px-6 h-fit py-4">
+          Create transaction
+        </PrimaryButton>
+      </div>
+
+      <div className="grid grid-cols-3 gap-4 mt-6">
+        <Card className="p-6" amount={22400} title="Escrow balance" />
+        <Card
+          className="p-6"
+          amount={37}
+          title="Total transactions "
+          button={
+            <PrimaryOutline className="w-fit px-5 text-[#D946EF]">
+              View all
+            </PrimaryOutline>
+          }
+        />
+
+        <Card
+          className="p-6"
+          amount={2}
+          title="Open disputes"
+          button={
+            <PrimaryOutline className="w-fit px-5 text-[#D946EF]">
+              View all
+            </PrimaryOutline>
+          }
+        />
+      </div>
+
+      <div className="grid grid-cols-3 gap-4 mt-6">
+        <div className="col-span-1">
+          <DoughnutChart />
+        </div>
+      </div>
+    </section>
+  );
 }
