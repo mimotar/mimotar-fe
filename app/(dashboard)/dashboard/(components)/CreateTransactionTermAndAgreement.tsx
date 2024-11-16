901dfbc48b59930ff1aad2f635e28a26950e247b
josephDev123 <josephuzuegbu55@gmail.com>
2024-11-16 11:42:28 +0100

    created Term and agreement on create transaction

--- app/(dashboard)/dashboard/(components)/CreateTransactionTermAndAgreement.tsx
+++ app/(dashboard)/dashboard/(components)/CreateTransactionTermAndAgreement.tsx
@@ -2,13 +2,15 @@
 
 import PrimaryButton, { PrimaryOutline } from "@/app/commons/PrimaryButtons";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
-import TransactionDetailSchema, {
-  TransactionDetailSchemaType,
+import {
+  TermAndAgreementSchema,
+  TermAndAgreementSchemaType,
 } from "@/lib/schemas/createTransactionSchema";
 import { setIsOpen, setStage } from "@/lib/slices/createTransactionStateSlice";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { IoMdArrowBack } from "react-icons/io";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
 
 export default function CreateTransactionTermAndAgreement() {
   const getCreateTransactionStateModal = useAppSelector(
@@ -19,13 +21,13 @@ export default function CreateTransactionTermAndAgreement() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<TransactionDetailSchemaType>({
-    resolver: zodResolver(TransactionDetailSchema),
+  } = useForm<TermAndAgreementSchemaType>({
+    resolver: zodResolver(TermAndAgreementSchema),
   });
 
-  const handleNext = (data: TransactionDetailSchemaType) => {
+  const handleNext = (data: TermAndAgreementSchemaType) => {
     console.log(data);
-    dispatch(setStage(2));
+    dispatch(setStage(3));
   };
   return (
     <section className="flex flex-col mx-auto sm:w-[580px] w-[90%]">
@@ -43,80 +45,177 @@ export default function CreateTransactionTermAndAgreement() {
           className="flex flex-col 2xl:gap-8 gap-4"
         >
           <div className="flex flex-col">
-            <label htmlFor="amount">Amount</label>
-            <input
-              type="text"
-              {...register("amount")}
-              id="amount"
-              placeholder="$ 40"
-              className="p-2 rounded-md border"
-            />
-            {errors.amount && (
-              <small className="text-red-400">{errors.amount.message}</small>
-            )}
-          </div>
+            <h1 className="inline-flex gap-2 items-center text-neutral-600 mb-2 font-medium">
+              Who will pay the escrow fee?
+              <AiOutlineExclamationCircle
+                title="Who will pay the escrow fee"
+                className="cursor-pointer"
+              />
+            </h1>
+            <div className="inline-flex gap-2 justify-between items-center">
+              <div className="inline-flex gap-2 items-center">
+                <input
+                  type="radio"
+                  {...register("escrowFeePayer")}
+                  id="escrowFeePayer_buyer"
+                  value={"Buyer (100%)"}
+                  className="p-2 rounded-md border w-5 h-5 cursor-pointer accent-purple-800"
+                />
 
-          <div className="flex flex-col">
-            <label htmlFor="transaction_description ">
-              Transaction description
-            </label>
-            <textarea
-              {...register("transaction_description")}
-              rows={4}
-              id="transaction_description"
-              placeholder="What is this payment for?"
-              className="p-2 rounded-md border"
-            ></textarea>
-            {errors.transaction_description && (
+                <label
+                  htmlFor="escrowFeePayer_buyer"
+                  className="text-neutral-900"
+                >
+                  Buyer (100%)
+                </label>
+              </div>
+              <div className="inline-flex gap-2 items-center">
+                <input
+                  type="radio"
+                  {...register("escrowFeePayer")}
+                  id="escrowFeePayer_seller"
+                  value="Seller (100%)"
+                  className="p-2 rounded-md border w-5 h-5 cursor-pointer accent-purple-800"
+                />
+
+                <label
+                  htmlFor="escrowFeePayer_seller"
+                  className="text-neutral-900"
+                >
+                  Seller (100%)
+                </label>
+              </div>
+              <div className="inline-flex gap-2 items-center">
+                <input
+                  type="radio"
+                  {...register("escrowFeePayer")}
+                  id="escrowFeePayer_both"
+                  value="Both (50% - 50%)"
+                  className="p-2 rounded-md border w-5 h-5 cursor-pointer accent-purple-800"
+                />
+
+                <label
+                  htmlFor="escrowFeePayer_both"
+                  className="text-neutral-900"
+                >
+                  Both (50% - 50%)
+                </label>
+              </div>
+            </div>
+
+            {errors.escrowFeePayer && (
               <small className="text-red-400">
-                {errors.transaction_description.message}
+                {errors.escrowFeePayer.message}
               </small>
             )}
           </div>
 
           <div className="flex flex-col">
-            <h1 className="mb-2">Transaction type</h1>
-            <div className="inline-flex items-center gap-2">
-              <input
-                type="radio"
-                {...register("transaction_type")}
-                value="physical product"
-                id="physical_product"
-                className="w-5 h-5 accent-purple-800"
-              />
-              <label htmlFor="physical_product " className="text-sm">
-                Physical product
-              </label>
-            </div>
-
-            <div className="inline-flex items-center gap-2 mt-2">
-              <input
-                type="radio"
-                {...register("transaction_type")}
-                value="online product"
-                id="online_product"
-                className="w-5 h-5 accent-purple-800"
-              />
-              <label htmlFor="online_product " className="text-sm">
-                Online product
-              </label>
-            </div>
-            <div className="inline-flex items-center gap-2 mt-2">
-              <input
-                type="radio"
-                {...register("transaction_type")}
-                value="service"
-                id="service"
-                className="w-5 h-5 accent-purple-800"
-              />
-              <label htmlFor="service " className="text-sm">
-                Service
-              </label>
-            </div>
-
-            {errors.transaction_type && (
+            <label
+              htmlFor="inspection_period"
+              className="text-neutral-600 font-medium"
+            >
+              How long is the inspection period?
+            </label>
+            <input
+              type="text"
+              {...register("inspection_period")}
+              id="inspection_period"
+              placeholder="between 1 to 30 days"
+              className="p-3 rounded-md border"
+            />
+            {errors.inspection_period && (
               <small className="text-red-400">
-                {errors.transaction_type.message}
+                {errors.inspection_period.message}
+              </small>
+            )}
+          </div>
+
+          <div className="flex flex-col">
+            <h1 className="inline-flex gap-2 items-center text-neutral-600 mb-2 font-medium">
+              Who will pay shipping costs?
+              <span className="bg-neutral-100 rounded-full py-0.5 text-sm px-1.5">
+                Optional
+              </span>
+            </h1>
+            <div className="inline-flex gap-2 justify-between items-center">
+              <div className="inline-flex gap-2 items-center">
+                <input
+                  type="radio"
+                  {...register("shipping_cost_payer")}
+                  id="shippingCostPayer_buyer"
+                  value={"Buyer (100%)"}
+                  className="p-2 rounded-md border w-5 h-5 cursor-pointer accent-purple-800"
+                />
+
+                <label
+                  htmlFor="shippingCostPayer_buyer"
+                  className="text-neutral-900"
+                >
+                  Buyer (100%)
+                </label>
+              </div>
+              <div className="inline-flex gap-2 items-center">
+                <input
+                  type="radio"
+                  {...register("shipping_cost_payer")}
+                  id="shippingCostPayer_seller"
+                  value=" Seller (100%)"
+                  className="p-2 rounded-md border w-5 h-5 cursor-pointer accent-purple-800"
+                />
+
+                <label
+                  htmlFor="shippingCostPayer_seller"
+                  className="text-neutral-900"
+                >
+                  Seller (100%)
+                </label>
+              </div>
+              <div className="inline-flex gap-2 items-center">
+                <input
+                  type="radio"
+                  {...register("shipping_cost_payer")}
+                  id="shippingCostPayer_both"
+                  value="Both (50% - 50%)"
+                  className="p-2 rounded-md border w-5 h-5 cursor-pointer accent-purple-800"
+                />
+
+                <label
+                  htmlFor="shippingCostPayer_both"
+                  className="text-neutral-900"
+                >
+                  Both (50% - 50%)
+                </label>
+              </div>
+            </div>
+
+            {errors.shipping_cost_payer && (
+              <small className="text-red-400">
+                {errors.shipping_cost_payer.message}
+              </small>
+            )}
+          </div>
+
+          <div className="flex flex-col">
+            <label
+              htmlFor="additional_agreement "
+              className="text-neutral-600 font-medium flex items-center gap-2"
+            >
+              Additional agreement{" "}
+              <span className="bg-neutral-100 rounded-full py-0.5 text-sm px-1.5">
+                Optional
+              </span>
+            </label>
+            <textarea
+              {...register("additional_agreement")}
+              rows={4}
+              id="additional_agreement"
+              placeholder="Discuss with the other person before you add any terms."
+              className="p-2 rounded-md border"
+            ></textarea>
+            {errors.additional_agreement && (
+              <small className="text-red-400">
+                {errors.additional_agreement.message}
               </small>
             )}
           </div>
--- lib/schemas/createTransactionSchema.ts
+++ lib/schemas/createTransactionSchema.ts
@@ -17,3 +17,29 @@ export type TransactionDetailSchemaType = z.infer<
   typeof TransactionDetailSchema
 >;
 export default TransactionDetailSchema;
+
+// Term And Agreement Schema
+export const TermAndAgreementSchema = z.object({
+  escrowFeePayer: z.enum(
+    ["Buyer (100%)", "Seller (100%)", "Both (50% - 50%)"],
+    {
+      required_error: "You must select who will pay the escrow fee.",
+    }
+  ),
+  inspection_period: z
+    .string()
+    .regex(/^\d+$/, "Inspection period must be a number.")
+    .refine((value) => {
+      const number = parseInt(value, 10);
+      return number >= 1 && number <= 30;
+    }, "Inspection period must be between 1 and 30 days."),
+  shipping_cost_payer: z
+    .enum(["Buyer (100%)", "Seller (100%)", "Both (50% - 50%)"])
+    .optional(),
+  additional_agreement: z
+    .string()
+    .max(500, "Additional agreement must be less than 500 characters.")
+    .optional(),
+});
+
+export type TermAndAgreementSchemaType = z.infer<typeof TermAndAgreementSchema>;
