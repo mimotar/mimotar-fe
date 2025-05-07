3a32fec7203dbe14c09007cf145581124a2c77a6
josephDev123 <josephuzuegbu55@gmail.com>
2025-05-07 16:22:36 +0100

    axios instance and logic setup

--- lib/services/axiosService.ts
+++ lib/services/axiosService.ts
@@ -1,29 +1,48 @@
 import axios from "axios";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/authOptions";
+import { getSession } from "next-auth/react";
+import toast from "react-hot-toast";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const axiosService = axios.create({
-  baseURL: `${process.env.API_BASE_URL}`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
 });
 axiosService.defaults.headers.post["Content-Type"] = "application/json";
 
 axiosService.interceptors.request.use(async (AxiosRequestConfig) => {
-  const session: any = await getServerSession(authOptions);
-  let token;
-  if (session) {
-    token = session.jwt;
+  // const session: any = await getServerSession(authOptions);
+  const session = await getSession();
+  const token = session?.user.accessToken;
+  if (token) {
+    AxiosRequestConfig.headers.Authorization = `Bearer ${token}`;
   }
-  AxiosRequestConfig.headers.Authorization = `Bearer ${token}`;
+
   return AxiosRequestConfig;
 });
 axiosService.interceptors.response.use(
   (AxiosResponse) => {
     return AxiosResponse;
   },
-  (error) => {
-    try {
-    } catch (e) {}
-    throw error;
+  async (error) => {
+    // const originalRequest = error.config;
+    if (error.response.status === 401) {
+      // Redirect to login page if unauthenticated
+      toast.error("unauthenticated", { position: "top-right" });
+      const data = await signOut({ redirect: false, callbackUrl: "/" });
+      useRouter().push(data.url);
+
+      return Promise.reject(error);
+    }
+
+    if (error.response.status === 403) {
+      // Redirect to login page if unauthorized
+
+      toast.error("session expired", { position: "top-right" });
+      const data = await signOut({ redirect: false, callbackUrl: "/" });
+      useRouter().push(data.url);
+    }
   }
 );
 export default axiosService;
